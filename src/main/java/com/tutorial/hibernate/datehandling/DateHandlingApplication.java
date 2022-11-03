ece2f1a0f4e83a099f66039e7562bb3dd9bf723b
David Turner <david.s.turner@ehi.com>
2022-11-03 09:17:47 -0500

    adding initial files

---
+++ src/main/java/com/tutorial/hibernate/datehandling/DateHandlingApplication.java
@@ -0,0 +1,13 @@
+package com.tutorial.hibernate.datehandling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DateHandlingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DateHandlingApplication.class, args);
+	}
+
+}
